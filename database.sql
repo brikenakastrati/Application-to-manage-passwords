63b229308ea435bdc71799c98b5896c519c11c89
darisdr <116508636+darisdr@users.noreply.github.com>
2024-05-15 20:24:18 +0200

    tblpasswords added to database

--- database.sql
+++ database.sql
@@ -7,3 +7,10 @@ CREATE TABLE tblusers (
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE tblpasswords(
+    pwd_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    encrypted_pwd VARCHAR(255) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES tblusers(user_id)
+)
